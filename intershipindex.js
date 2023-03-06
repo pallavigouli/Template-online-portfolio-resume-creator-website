92d5ad9cd1e4d11c4a0d0fa4899e0856f60679af
pallavigouli <127080054+pallavigouli@users.noreply.github.com>
2023-03-06 12:49:23 +0530

    Add files via upload

---
+++ intershipindex.js
@@ -0,0 +1,7 @@
+// Create an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+
+// Loop through the array and log each number to the console
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
